939c64da04d7145beb1c61575237380453010df4
BETUANMINH <minhbthe176084@fpt.edu.vn>
2024-05-13 21:53:42 +0700

    Add Common CRUD

---
+++ Common/IDbRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAssignment1Group3.Common
+{
+    public interface IDbRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task InsertAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
