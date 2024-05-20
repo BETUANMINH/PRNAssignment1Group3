15fe8c35e4acf0313b2ad2a91ab6c7f747c02d46
indexy194 <duydao0221@gmail.com>
2024-05-20 20:19:29 +0700

    add default account

---
+++ State/DefaultAccount.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFAssignment1Group3.State
+{
+    public class DefaultAccount
+    {
+        public int Id { get; set; } = 1;
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Role {  get; set; } = 0;
+    }
+}
